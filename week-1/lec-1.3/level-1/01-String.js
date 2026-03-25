229f5d08db84448afd3f73f94ccdc64b52afb19a
Vinal Pawar <vinalpawar1@gmail.com>
2026-03-25 21:16:20 +0530

    uncomment

--- week-1/lec-1.3/level-1/01-String.js
+++ week-1/lec-1.3/level-1/01-String.js
@@ -1,33 +1,32 @@
-// let fname = "vinal";
-// console.log(fname.length);
+let fname = "vinal";
+console.log(fname.length);
 
-// let findIndex = "Hello World";
-// const value = findIndex.indexOf("World");
-// console.log(value);
+let findIndex = "Hello World";
+const value = findIndex.indexOf("World");
+console.log(value);
 
-// let findLastIndex = "Hello World World";
-// const value = findLastIndex.lastIndexOf("World");
-// console.log(value);
+let findLastIndex = "Hello World World";
+const value2 = findLastIndex.lastIndexOf("World");
+console.log(value2);
 
-// let str = "Hellow World";
-// const value = str.slice(0, 5);
-// console.log(value);
+let str = "Hellow World";
+const value3 = str.slice(0, 5);
+console.log(value3);
 
-// let str = "Hello World";
-// const value = str.replace("World", "JavaScript");
-// console.log(value);
+let str2 = "Hello World";
+const value4 = str.replace("World", "JavaScript");
+console.log(value4);
 
-// let str = "my name is vinal";
-// let value = str.split(" ");
-// console.log(value);
+let str3 = "my name is vinal";
+let value5 = str.split(" ");
+console.log(value5);
 
-// let str = "    vinal str    ";
-// let value = str.trim();
-// console.log(value)
+let str4 = "    vinal str    ";
+let value6 = str.trim();
+console.log(value6);
 
-// let str = "vinal STR";
-// let value1 = str.toLowerCase();
-// let value2 = str.toUpperCase();
-// console.log(`Lower Case: ${value1}`);
-// console.log(`Upper Case: ${value2}`);
-
+let str5 = "vinal STR";
+let value7 = str5.toLowerCase();
+let value8 = str5.toUpperCase();
+console.log(`Lower Case: ${value7}`);
+console.log(`Upper Case: ${value8}`);
