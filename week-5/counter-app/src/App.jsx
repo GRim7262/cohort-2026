f2fe704a148f23fc7bd36ded7116942fd3785b95
Vinal Pawar <vinalpawar1@gmail.com>
2026-07-02 19:43:45 +0530

    modified

--- week-5/counter-app/src/App.jsx
+++ week-5/counter-app/src/App.jsx
@@ -4,29 +4,47 @@ import "./App.css";
 function App() {
   const [todos, setTodos] = useState([
     {
-      title: "Test",
-      description: "TesT",
+      title: "Title",
+      description: "Description",
+      completed: false,
+    },
+    {
+      title: "Title-2",
+      description: "Description-2",
       completed: false,
     },
   ]);
 
-  return (
-    <>
-      {JSON.stringify(todos)}
-    </>
-  );
-}
-
-function CustomButton(props) {
-  function onClickHandler() {
-    props.setCount(props.count + 1);
+  function AddTodo() {
+    setTodos([
+      ...todos,
+      {
+        title: "New Title",
+        description: "New Description",
+        completed: false,
+      },
+    ]);
   }
 
   return (
     <>
-      <button onClick={onClickHandler}>Counter {props.count}</button>
+      <button onClick={AddTodo}>Add Todo</button>
+      {todos.map((todo, index) => (
+        <Todo key={index} title={todo.title} description={todo.description} />
+      ))}
     </>
   );
 }
+
+const Todo = ({ title, description }) => {
+  return (
+    <>
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </>
+  );
+};
 
 export default App;
